d99b229f9536433bde02a521bb373723ea88dae4
aimstarter <aimstarter01@gmail.com>
2022-12-07 00:39:35 +0900

    cors

--- src/main/java/com/example/hodolog/config/WebConfig.java
+++ src/main/java/com/example/hodolog/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080");
+                .allowedOrigins("http://192.168.1.10:8081");
     }
 
 }
